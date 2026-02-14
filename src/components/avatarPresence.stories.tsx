5d832ddcc8963b61983c3608404e9f76ec2a1d25
Francesco de Lorenzo <francescodelorenzo44@gmail.com>
2026-02-14 17:07:43 +0200

    Update text in stories

--- src/components/avatarPresence.stories.tsx
+++ src/components/avatarPresence.stories.tsx
@@ -17,9 +17,9 @@ type Story = StoryObj<typeof meta>;
 export const AvatarPresenceTest: Story = {
   args: {
     avatars: [
-      {avatar: {color: "#FF0000", initial: "Fx"}, user: "userF@mail.com"},
-      {avatar: {color: "#FFFF00", initial: "Fx"}, user: "userF@mail.com"},
-      {avatar: {color: "#FF3333", initial: "Fx"}, user: "userawddwadsddddafcacF@mail.com"},
+      {avatar: {color: "#FF0000", initial: "Fx"}, text: "userF@mail.com"},
+      {avatar: {color: "#FFFF00", initial: "Fx"}, text: "userF@mail.com"},
+      {avatar: {color: "#FF3333", initial: "Fx"}, text: "userawddwadsddddafcacF@mail.com"},
     ]
   },
 };
